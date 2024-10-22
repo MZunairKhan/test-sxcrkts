0caca6072f7922c378be2caff9664adb1d3dc752
MZunairKhan <khanzunair15@outlook.com>
2024-10-22 11:39:49 +0500

    Add HTTP service

---
+++ angular-test/src/app/service/deadline.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, map, catchError, throwError } from 'rxjs';
+
+import { DeadlineResponse } from '../model/deadlineResponse';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DeadlineService {
+  private readonly apiUrl = '/api/deadline';
+
+  constructor(private http: HttpClient) {}
+
+  getSecondsLeft(): Observable<number> {
+    return this.http.get<DeadlineResponse>(this.apiUrl).pipe(
+      map(response => response.secondsLeft),
+      catchError(error => {
+        console.error('Error fetching deadline:', error);
+        return throwError(() => new Error('Failed to fetch deadline'));
+      })
+    );
+  }
+}
